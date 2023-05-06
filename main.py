b275c3cc3ee2f30e03a9e6a2b7dabd1623ad182b
Sun XiaoXiang <hereaream@gmail.com>
2023-05-07 02:01:03 +0800

    add blog comments

--- main.py
+++ main.py
@@ -4,5 +4,22 @@ app = FastAPI()
 
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World!"}
+async def index():
+    return {"data": "blog list"}
+
+
+@app.get("/blog/{id}")
+async def show(id):
+    # fetch blog with id = id
+    return {"data": id}
+
+
+@app.get("/blog/{id}/comments")
+async def comments(id):
+    # fetch comments of blog with id = id
+    return {"data": id}
+
+
+@app.get("/about")
+async def about():
+    return {"about": "This is a gpt fastapi demo"}
